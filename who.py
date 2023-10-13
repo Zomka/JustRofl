6cf853f68de86173511cf64bf89be5479e2529df
Zomka <zomkahelp@gmail.com>
2023-10-14 00:16:26 +0400

    who?
    who?who?who?who?who?who?

---
+++ who.py
@@ -0,0 +1,18 @@
+import time
+
+x = 1000
+
+time.sleep(0.2)
+print('Поздравляю')
+time.sleep(0.3)
+print('Вы - гуль')
+time.sleep(0.3)
+print('')
+time.sleep(0.5)
+
+while x > 7:
+     print(f"{x} - 7 = {x-7}")
+     x -= 7
+     time.sleep(0.05)
+
+print('Теперь вы дединсайдик')
